568a564bbbea37fae20424a22c333305bc4f2d91
42290931@qq.com <42290931@qq.com>
2017-07-11 13:52:52 +0800

    t

--- modules/cn/views/index/index.php
+++ modules/cn/views/index/index.php
@@ -18,7 +18,56 @@
         </div>
         <div class="js-silder-ctrl"></div>
     </div>
-    <!-- <img src="/cn/index/banner_01.jpg"> -->
+</div>
+<div class="pro-list-wrap">
+    <div class="pro-list-div">
+        <div class="pic">
+            <img src="/cn/index/1.jpg">
+        </div>
+        <div class="pic-content">
+            <ul>
+                <li><a href="">2017年世界大战即将开始印度吃翔结束</a></li>
+                <li><a href="">2017年世界大战即将开始印度吃翔结束</a></li>
+                <li><a href="">2017年世界大战即将开始印度吃翔结束</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="pro-list-div">
+        <div class="pic">
+            <img src="/cn/index/2.jpg">
+        </div>
+        <div class="pic-content">
+            <ul>
+                <li><a href="">2017年世界大战即将开始印度吃翔结束</a></li>
+                <li><a href="">2017年世界大战即将开始印度吃翔结束</a></li>
+                <li><a href="">2017年世界大战即将开始印度吃翔结束</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="pro-list-div">
+        <div class="pic">
+            <img src="/cn/index/3.jpg">
+        </div>
+        <div class="pic-content">
+            <ul>
+                <li><a href="">2017年世界大战即将开始印度吃翔结束</a></li>
+                <li><a href="">2017年世界大战即将开始印度吃翔结束</a></li>
+                <li><a href="">2017年世界大战即将开始印度吃翔结束</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="pro-list-div">
+        <div class="pic">
+            <img src="/cn/index/4.jpg">
+        </div>
+        <div class="pic-content">
+            <ul>
+                <li><a href="">2017年世界大战即将开始印度吃翔结束</a></li>
+                <li><a href="">2017年世界大战即将开始印度吃翔结束</a></li>
+                <li><a href="">2017年世界大战即将开始印度吃翔结束</a></li>
+            </ul>
+        </div>
+    </div>
 </div>
 <?php
     AppAsset::addCss($this, Yii::$app->request->baseUrl."/widget/jQueryLbWy/index.css");
