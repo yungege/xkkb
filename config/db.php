36c921c128f0c940cb0b8fb6877511a0ac5ddfed
42290931@qq.com <42290931@qq.com>
2017-07-27 22:20:35 +0800

    t

--- config/db.php
+++ config/db.php
@@ -2,7 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=47.93.160.163;dbname=xkkb',
+    // 'dsn' => 'mysql:host=47.93.160.163;dbname=xkkb',
+    'dsn' => 'mysql:host=192.168.1.44;dbname=xkkb',
     'username' => 'xkkb',
     'password' => 'xkkb123456',
     'charset' => 'utf8',
