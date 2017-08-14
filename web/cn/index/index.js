52e05ba90f04ef9b599b4d38927e4bd39989a22c
42290931@qq.com <42290931@qq.com>
2017-08-14 23:03:56 +0800

    t

--- web/cn/index/index.js
+++ web/cn/index/index.js
@@ -1,71 +1,77 @@
-var HoverImg = {
+$(function(){
+    var HoverImg = {
 
-    init: function (){
-        this.getDom();
-        this.changeImg();
-        this.topPos();
-    },
+        init: function (){
+            this.getDom();
+            this.changeImg();
+            this.topPos();
+        },
 
-    getDom: function (){
-        this.picLis = $('.pic-content ul li');
-        this.picCover = $('.pro-list-div');
-    },
+        getDom: function (){
+            this.picLis = $('.pic-content ul li');
+            this.picCover = $('.pro-list-div');
+        },
 
-    changeImg: function (){
-        var me = this,
-            timer = null;
-        me.picLis.unbind().bind('mouseenter', function(){
-            var ms = 1,
-                picArea = $(this).parent().parent().prev().children('img'),
-                picUrl = $(this).attr('data-img');
-            if(timer !== null){
-                clearInterval(timer);
+        changeImg: function (){
+            var me = this,
                 timer = null;
-            }
-            timer = setInterval(function(){
-                if(ms == 1){
-                    if(picUrl){
-                        picArea.attr('src', picUrl);
-                    }
+            me.picLis.unbind().bind('mouseenter', function(){
+                var ms = 1,
+                    picArea = $(this).parent().parent().prev().children('img'),
+                    picUrl = $(this).attr('data-img');
+                if(timer !== null){
                     clearInterval(timer);
                     timer = null;
                 }
-                else{
-                    ms ++;
-                }
-                
-            }, 250);
-            
-        });
-    },
-
-    topPos: function(){
-        var me = this;
-            
-        me.picCover.unbind().bind('mouseenter', function(){
-            var picDiv = $(this).children('.pic');
-            picDiv.stop().animate({
-                'bottom': '110px',
-            },{ 
-                easing: 'easeOutExpo', 
-                duration: 600,
-            });
-            
-        }).bind('mouseleave', function(){
-            var picDiv = $(this).children('.pic');
-            picDiv.stop().animate({
-                'bottom': '0px',
-            },{ 
-                easing: 'easeOutExpo', 
-                duration: 600,
-            });
+                timer = setInterval(function(){
+                    if(ms == 1){
+                        if(picUrl){
+                            picArea.attr('src', picUrl);
+                        }
+                        clearInterval(timer);
+                        timer = null;
+                    }
+                    else{
+                        ms ++;
+                    }
                     
-        });
-    },
+                }, 250);
+                
+            });
+        },
 
-};
+        topPos: function(){
+            var me = this;
+                
+            me.picCover.unbind().bind('mouseenter', function(){
+                var picDiv = $(this).children('.pic');
+                picDiv.stop().animate({
+                    'bottom': '110px',
+                },{ 
+                    easing: 'easeOutExpo', 
+                    duration: 600,
+                });
+                
+            }).bind('mouseleave', function(){
+                var picDiv = $(this).children('.pic');
+                picDiv.stop().animate({
+                    'bottom': '0px',
+                },{ 
+                    easing: 'easeOutExpo', 
+                    duration: 600,
+                });
+                        
+            });
+        },
 
-$(function(){
+    };
+    
+    HoverImg.init();
+})
+
+
+window.onload = function(){
+
     $(".js-silder").silder({
         auto: true,//自动播放，传入任何可以转化为true的值都会自动轮播
         speed: 30,//轮播图运动速度
@@ -75,19 +81,4 @@ $(function(){
         activeClass: "active",//小的控制按钮激活的样式，不包括作用两边，默认active
         bottomCtrl: true,
     });
-})
-
-// window.onload = function(){
-
-//     $(".js-silder").silder({
-//         auto: true,//自动播放，传入任何可以转化为true的值都会自动轮播
-//         speed: 30,//轮播图运动速度
-//         sideCtrl: false,//是否需要侧边控制按钮
-//         defaultView: 0,//默认显示的索引
-//         interval: 5000,//自动轮播的时间，以毫秒为单位，默认3000毫秒
-//         activeClass: "active",//小的控制按钮激活的样式，不包括作用两边，默认active
-//         bottomCtrl: true,
-//     });
-
-//     HoverImg.init();
-// }
+}
